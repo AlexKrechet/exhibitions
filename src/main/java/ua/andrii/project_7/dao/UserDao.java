021d6f35fd309efdc386572f88b6a8abaca1b983
Alexkre4et <45433324+Alexkre4et@users.noreply.github.com>
2019-08-03 17:34:59 +0300

    Dao level interfaces were added

---
+++ src/main/java/ua/andrii/project_7/dao/UserDao.java
@@ -0,0 +1,21 @@
+package ua.andrii.project_7.dao;
+
+import java.util.List;
+
+public interface UserDao<T> {
+    Long create(T t);
+
+    T read(Long id);
+
+    boolean update(T t);
+
+    boolean delete(T t);
+
+    List<T> findAll();
+
+    T getUser(String login, String password);
+
+    boolean hasUser(String login);
+
+    List<T> getVisitors();
+}
